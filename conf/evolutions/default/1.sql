a2a9e22c84d973f82a1dcff0ded85205b2147585
katsu <katsu@192.168.1.103>
2020-01-25 00:40:44 +0900

    [update]evolutionで自動生成されたDB生成クエリ

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,49 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table t_relations (
+  id                            bigserial not null,
+  editor                        varchar(255),
+  token_id                      bigint,
+  one_tip_id                    bigint,
+  other_tip_id                  bigint,
+  create_time                   timestamp not null,
+  update_time                   timestamp not null,
+  update_count                  integer not null,
+  constraint pk_t_relations primary key (id)
+);
+
+create table t_tips (
+  id                            bigserial not null,
+  editor                        varchar(255),
+  token_id                      bigint,
+  subject                       varchar(255),
+  note                          varchar(255),
+  tags                          varchar(255),
+  create_time                   timestamp not null,
+  update_time                   timestamp not null,
+  update_count                  integer not null,
+  constraint pk_t_tips primary key (id)
+);
+
+create table t_tokens (
+  id                            bigserial not null,
+  editor                        varchar(255),
+  token                         varchar(255),
+  create_time                   timestamp not null,
+  update_time                   timestamp not null,
+  update_count                  integer not null,
+  constraint pk_t_tokens primary key (id)
+);
+
+
+# --- !Downs
+
+drop table if exists t_relations cascade;
+
+drop table if exists t_tips cascade;
+
+drop table if exists t_tokens cascade;
+
